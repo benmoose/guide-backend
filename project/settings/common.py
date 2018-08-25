78cfcc101952edc31a3f3fe67f28f2ca721718a7
Ben Hadfield <benjohnhadfield@gmail.com>
2018-08-25 13:09:52 +0100

    add place app

--- project/settings/common.py
+++ project/settings/common.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'guide.urls'
+ROOT_URLCONF = 'project.urls'
 
 TEMPLATES = [
     {
@@ -75,7 +75,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'guide.wsgi.application'
+WSGI_APPLICATION = 'project.wsgi.application'
 
 
 # Database
--- project/wsgi.py
+++ project/wsgi.py
@@ -12,6 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'guide.settings.production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings.production')
 
 application = get_wsgi_application()
